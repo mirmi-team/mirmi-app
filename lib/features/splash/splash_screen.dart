35301a5d791363ac2da58346f119290ae6e29b86
이제호 <je23ss@naver.com>
2026-06-23 22:38:28 +0900

    feat: 스플래시 화면 애니메이션 타임 조정

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -21,7 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _startAnimation() async {
     // 로고만 1초
-    await Future.delayed(const Duration(milliseconds: 850));
+    await Future.delayed(const Duration(milliseconds: 1000));
 
     setState(() {
       logoOffset = -45; // 위로 이동
